2edf5bdb31f2955d87097ed1ddcf9f2113cbbe83
Dima Snytin <dimasnytin@gmail.com>
2022-05-26 13:58:53 +0300

    add section Home - mobile menu - add onscroll js window - add info-container

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// скрипт для меню
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = function() {
+  menu.classList.toggle('fa-times');
+  header.classList.toggle('active');
+}
+
+window.onscroll = function () {
+  menu.classList.toggle('fa-times');
+  header.classList.toggle('active');
+}
